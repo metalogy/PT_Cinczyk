33b0fcaad14de3d84ebfce9dbabc26cadc52f47a
Michal <metalogy@wp.pl>
2021-09-06 23:42:23 +0200

    Initial commit

---
+++ src/GUI/Controller.java
@@ -0,0 +1,20 @@
+package GUI;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Controller {
+    @FXML
+    private Button loginButton;
+    @FXML
+    private Button guestButton;
+
+    @FXML
+    private void toGame(ActionEvent event) {
+
+    }
+
+
+}
