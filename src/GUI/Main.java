1f1a0da88817234f0ba4fc1a9ddff14bc4951a46
metalogy <metalogy@wp.pl>
2021-09-08 13:30:29 +0200

    Game board added

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -11,6 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("LoginScreen.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("gameBoard.fxml"));
         primaryStage.setTitle("Chinczyk");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
