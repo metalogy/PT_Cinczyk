00d034bb8c7c82590de4e35acdc0ff1a571d7548
Szymon Wzietek <sz.wzietek@gmail.com>
2021-09-17 10:11:56 +0200

    Utworzenie brancha

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("LoginScreen.fxml"));
         primaryStage.setTitle("Chinczyk");
         primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        primaryStage.show();//
     }
 
 
