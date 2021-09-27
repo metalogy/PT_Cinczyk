e7ee5a8329bbcf4513b38b01f80d6f17ccad19ae
Szynaman123 <45926130+Szynaman123@users.noreply.github.com>
2021-09-27 16:01:47 +0200

    Login and warning added

--- src/GUI/PlayerScreen.java
+++ src/GUI/PlayerScreen.java
@@ -12,11 +12,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class PlayerScreen implements Initializable {
@@ -29,11 +32,21 @@ public class PlayerScreen implements Initializable {
     private ChoiceBox<String>choicebox3;
     @FXML
     private ChoiceBox<String>choicebox4;
-
+    @FXML
+    private TextField textField1;
+    @FXML
+    private TextField textField2;
+    @FXML
+    private TextField textField3;
+    @FXML
+    private TextField textField4;
+    @FXML
+    private Text Warning;
     @FXML
     private Button startButton;
     private ArrayList<ChoiceBox<String>>Choiceboxes;
     private Game game;
+    private ArrayList<TextField>TextFields;
 
     private String[] colour={"red","blue","yellow","green"};
 
@@ -46,10 +59,15 @@ public PlayerScreen() {
     @FXML
     private void startGame(ActionEvent event) throws IOException {
     Choiceboxes=new ArrayList<ChoiceBox<String>>();
+    TextFields=new ArrayList<TextField>();
     Choiceboxes.add(choicebox1);
     Choiceboxes.add(choicebox2);
     Choiceboxes.add(choicebox3);
     Choiceboxes.add(choicebox4);
+    TextFields.add(textField1);
+    TextFields.add(textField2);
+    TextFields.add(textField3);
+    TextFields.add(textField4);
         int red=0,blue = 0,yellow=0,green=0;
         for(ChoiceBox<String>choicebox : Choiceboxes){
             switch (choicebox.getValue()){
@@ -71,23 +89,31 @@ public PlayerScreen() {
         }
         if(red<2&&blue<2&&green<2&&yellow<2){
             ArrayList<Player> playerArrayList=new ArrayList<Player>();
-            if(red==1){
-                Player player = new Player(PawnsColorEnum.Red, "red");
-                playerArrayList.add(player);
-            }
-            if (blue==1){
-                Player player = new Player(PawnsColorEnum.Blue, "blue");
-                playerArrayList.add(player);
-            }
-            if (yellow==1){
-                Player player = new Player(PawnsColorEnum.Yellow, "yellow");
-                playerArrayList.add(player);
-            }
-            if (green==1){
-                Player player = new Player(PawnsColorEnum.Green, "green");
-                playerArrayList.add(player);
-            }
+            int i =0;
+            for(ChoiceBox<String>choicebox : Choiceboxes){
+                if(!Objects.equals(choicebox.getValue(), "None")) {
+                    Player player= new Player(PawnsColorEnum.Red, TextFields.get(i).getText());
+                    switch (choicebox.getValue()) {
+                        case "red":
+                            player = new Player(PawnsColorEnum.Red, TextFields.get(i).getText());
+                            break;
+                        case "green":
+                            player = new Player(PawnsColorEnum.Green, TextFields.get(i).getText());
+                            break;
+                        case "blue":
+                            player = new Player(PawnsColorEnum.Blue, TextFields.get(i).getText());
+                            break;
+                        case "yellow":
+                            player = new Player(PawnsColorEnum.Yellow, TextFields.get(i).getText());
+                            break;
+                        default:
+                            break;
+                    }
 
+                    i++;
+                    playerArrayList.add(player);
+                }
+            }
             FXMLLoader loader = new FXMLLoader(getClass().getResource("GameBoard.fxml"));
             Game game = new Game("TestGame",playerArrayList);
             GameBoardController controller=new GameBoardController(game);
@@ -100,7 +126,7 @@ public PlayerScreen() {
 
 
 
-        }
+        }else Warning.setVisible(true);
 
 
     }
