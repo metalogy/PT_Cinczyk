3f55afdcfdf9574f0361e189154e0d1373ff6a80
Szynaman123 <45926130+Szynaman123@users.noreply.github.com>
2021-09-28 19:43:40 +0200

    Statistics Screen

---
+++ src/GUI/StatisticsController.java
@@ -0,0 +1,23 @@
+package GUI;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.Button;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+
+public class StatisticsController implements Initializable {
+    @FXML
+    private TextArea textArea;
+    @FXML
+    private Button button;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
